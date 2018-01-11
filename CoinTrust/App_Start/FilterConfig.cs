83124e8b3cc03e3c35ae159ee4a21263b5bf14f5
F1026120 <mays2288@gmail.com>
2018-01-12 01:09:48 +0800

    加入專案檔案。

---
+++ CoinTrust/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CoinTrust
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
