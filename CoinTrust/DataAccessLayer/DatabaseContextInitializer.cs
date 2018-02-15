ab4efe6f9a18ccaa26c2c94dbc20864fcc631824
iattempt <iattempt.net@gmail.com>
2018-02-16 00:48:45 +0800

    init seed function

--- CoinTrust/DataAccessLayer/DatabaseContextInitializer.cs
+++ CoinTrust/DataAccessLayer/DatabaseContextInitializer.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using CoinTrust.Models;
 
 namespace CoinTrust.DataAccessLayer
 {
-    public class DatabaseContextInitializer
+    public class DatabaseContextInitializer : DropCreateDatabaseAlways<DatabaseContext>
     {
+        protected override void Seed(CoinTrust.DataAccessLayer.DatabaseContext context)
+        {
+            //This method will be called after migrating to the latest version.
+
+            //You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //to avoid creating duplicate seed data.
+
+            //This method will be called after migrating to the latest version.
+
+            //You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //to avoid creating duplicate seed data.
+
+        }
     }
 }
