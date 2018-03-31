f74596233bc83a15932b572a92c3b664d1444e88
F1026120 <mays2288@gmail.com>
2018-04-01 01:06:55 +0800

    update boostrap to 4.0.0

--- CoinTrust/App_Start/BundleConfig.cs
+++ CoinTrust/App_Start/BundleConfig.cs
@@ -22,7 +22,8 @@ namespace CoinTrust
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
