817568f0a223bc3463b8323578bfdce828d3e6ef
F1026120 <mays2288@gmail.com>
2018-04-04 16:55:10 +0800

    add model NameDisplay attributes

--- CoinTrust/Models/Account.cs
+++ CoinTrust/Models/Account.cs
@@ -4,28 +4,33 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace CoinTrust.Models
 {
     public class Account
     {
         [Key]
+        [DisplayName("使用者信箱")]
         [Required]
         [EmailAddress]
         public string AccountId { get; set; }
 
         [Required]
+        [DisplayName("密碼")]
         public string Password { get; set; }
 
         [NotMapped]
         //[Compare("Password", ErrorMessage = "密碼不一致")]
+        [DisplayName("確認密碼")]
         public string ConfirmPassword { get; set; }
 
         [Required]
         [MinLength(10, ErrorMessage = "手機號碼格式錯誤"), MaxLength(10, ErrorMessage = "手機號碼格式錯誤")]
         [RegularExpression("[0-9]{10}", ErrorMessage = "手機號碼格式錯誤")]
+        [DisplayName("手機號碼")]
         public string Phone { get; set; }
-        
+
         [Required]
         public bool Certified { get; set; }
 
